9c1d5f2ce4e005c2591291d8a6df1d94fa1aaab0
JonChamplain <47443316+JonChamplain@users.noreply.github.com>
2020-12-08 22:31:37 -0500

    sanitize google req in api.js

--- Public/api.js
+++ Public/api.js
@@ -4,7 +4,7 @@ fetch('./apikey')
   .then((response) => {
     return response.text()
   }).then((apiKey) => {
-    let googleReq = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
+    let googleReq = encodeURIComponent(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`);
     script.setAttribute('src', googleReq)
     script.setAttribute('async', true)
     script.setAttribute('defer', true)
