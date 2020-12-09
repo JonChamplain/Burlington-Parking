735aad08e9607d17136d71969de772e8bf3d4937
JonChamplain <47443316+JonChamplain@users.noreply.github.com>
2020-12-08 23:03:37 -0500

    still troubleshooting

--- Public/api.js
+++ Public/api.js
@@ -4,12 +4,14 @@ fetch('./apikey')
   .then((response) => {
     return response.text()
   }).then((apiKey) => {
-    apiKey = encodeURIComponent(apiKey)
     let googleReq =`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
     script.setAttribute('src', googleReq)
     script.setAttribute('async', true)
     script.setAttribute('defer', true)
     document.body.appendChild(script)
+    console.log(script)
+    script = encodeURIComponent(script)
+    console.log(script)
     return script
   })
 
